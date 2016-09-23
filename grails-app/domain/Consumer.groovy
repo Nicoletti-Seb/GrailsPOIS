8d2a3243e8cc7583ed457cc84243165090b97ecb
leopaul29 <leopaulmartin@hotmail.fr>
2016-09-23 15:17:47 +0200

    to debug

---
+++ grails-app/domain/Consumer.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by LeoPaul on 23/09/2016.
+ */
+class Consumer {
+    String nom
+
+    static hasOne = [Role]
+}
---
+++ grails-app/domain/Role.groovy
@@ -0,0 +1,6 @@
+/**
+ * Created by LeoPaul on 23/09/2016.
+ */
+class Role {
+    String name
+}
