e19ea3650a5c73bbc8cd5b5f809bbdc37023b1ad
Sebastien Nicoletti <sebastien.nicoletti@53js.fr>
2016-09-27 10:22:28 +0200

    Security: create admin user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,6 @@
 import com.projetpois.user.Role
 import com.projetpois.user.User
+import com.projetpois.user.UserRole
 
 class BootStrap {
 
@@ -7,12 +8,14 @@ class BootStrap {
 
         def adminRole = new Role('ROLE_ADMIN').save()
         def userRole = new Role('ROLE_USER').save()
+        def moderatorRole = new Role('ROLE_MODERATOR').save()
 
         def testUser = new User('root', 'azerty@gmail.com', 'root').save()
-
+        UserRole.create testUser, adminRole, true
 
         assert User.count() == 1
-        assert Role.count() == 2
+        assert Role.count() == 3
+        assert UserRole.count() == 1
     }
     def destroy = {
     }
